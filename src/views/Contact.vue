3b22fb98f52fcc33c2a1dff30aa8e9092caca36a
Scarletthedragon <sarlote.termane@gmail.com>
2026-05-17 08:00:35 +0300

    online?

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -63,7 +63,7 @@ const submitForm = async () => {
   if (!validateForm()) return
 
   try {
-    const response = await fetch('http://localhost:8000/api/contact', {
+    const response = await fetch(`${import.meta.env.VITE_API_URL ?? 'http://localhost:8000'}/api/contact`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
