216c356a3843c424c40881ee76d20c75178822ed
Scarletthedragon <sarlote.termane@gmail.com>
2026-05-21 18:09:29 +0300

    fix addmint

--- laravel-backend/database/seeders/MakeAdminSeeder.php
+++ laravel-backend/database/seeders/MakeAdminSeeder.php
@@ -2,17 +2,22 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class MakeAdminSeeder extends Seeder
 {
     public function run()
     {
-        $user = User::where('email', 'admin@dragons.com')->first();
-        if ($user) {
-            $user->is_admin = true;
-            $user->save();
-        }
+        User::updateOrCreate(
+            ['email' => 'admin@dragons.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin123'),
+                'is_admin' => true,
+            ]
+        );
     }
 }
