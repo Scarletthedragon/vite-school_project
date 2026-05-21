9a285266d0b61e43650c61948ba9673df7cc8f99
Scarletthedragon <sarlote.termane@gmail.com>
2026-05-21 20:29:54 +0300

    plz

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -198,4 +198,4 @@ onMounted(() => fetchLeaderboard())
 
 <button @click="makeAdmin" style="margin-top: 1rem; padding: 0.7rem 1.5rem; background: #e67e22; color: #fff; border: none; border-radius: 6px; font-weight: bold; cursor: pointer;">
   Become Admin
-</button>
+ 
